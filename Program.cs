f1f888f800e7bbd61b51832d8eb2329ddc5e6836
devoisaurus <adevois@gmail.com>
2016-07-11 21:21:29 -0500

    cheer working

--- Program.cs
+++ Program.cs
@@ -13,22 +13,53 @@ namespace Cheers
             //Variables section
             String prompt = ">  ";
             String cheer_prefix = "Give me a";
+            String requires_an = "aeiolnfshrmx";
 
 
             Console.WriteLine("What's your name?");
             Console.Write(prompt);
-            String user_name = Console.ReadLine(); //Read from terminal save to a variable
+            String user_name = Console.ReadLine().ToLower(); //Read from terminal save to a variable
 
+            //First Refactoring removing the "else"
+            /*
             for (int i = 0; i < user_name.Length; i++)
             {
+                String cheer_prefix = "Give me a"; //Resets the cheer_prefix variable every iteration through for loop
+                if (requires_an.IndexOf(user_name[i]) > -1) {
+                    cheer_prefix += "n";
+                }
                 Console.WriteLine(cheer_prefix + "..." + user_name[i]);
+            } */
+
+            //Final refactoring using Contains
+            for (int i = 0; i < user_name.Length; i++)
+            {
+                if (requires_an.Contains(user_name[i])) {
+                    Console.WriteLine(cheer_prefix + "n" + "..." + user_name[i]);
+                } else {
+                    Console.WriteLine(cheer_prefix + "..." +user_name[i]);
+                }
             }
+
+            
+            //Original solution using "IndexOf"
+            /*for (int i = 0; i < user_name.Length; i++) {
+                if (requires_an.IndexOf(user_name[i]) > -1) {
+                    //Then it's in the requires_an array!!!!
+                    Console.WriteLine(cheer_prefix + "n" + "..." + user_name[i]);
+                } else {
+                    //Does not require an "an"
+                    Console.WriteLine(cheer_prefix + "..." + user_name[i]);
+                }
+                
+            }
+            */
 
             //user_name.ToUpper() is turning the user's name to uppercase
             //ToUpper() is functionality specific to strings.
-            //ToUpper() accepts no arguments
+            //ToUpper() accepts no arguments 
 
-            Console.WriteLine(user_name.ToUpper() + " is.. GRAND!");
+            Console.WriteLine(user_name.ToUpper() + " is.. GRAND!"); 
         }
     }
 }
