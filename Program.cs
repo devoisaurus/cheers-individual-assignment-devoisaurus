74c94a39313be76e17027720b507281b141c1099
devoisaurus <adevois@gmail.com>
2016-07-11 20:21:37 -0500

    added for loop to spell name

--- Program.cs
+++ Program.cs
@@ -13,10 +13,15 @@ namespace Cheers
             Console.Write("> ");
             String user_name = Console.ReadLine(); //Read from terminal save to a variable
 
+            for (int i = 0; i < user_name.Length; i++)
+            {
+                Console.WriteLine("Give me a..." + user_name[i]);
+            }
+
             //user_name.ToUpper() is turning the user's name to uppercase
             //ToUpper() is functionality specific to strings.
             //ToUpper() accepts no arguments
-            
+
             Console.WriteLine(user_name.ToUpper() + " is.. GRAND!");
         }
     }
